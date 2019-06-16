1b33e6960563efb1fe2fc32778d77f919dbd0563
Takashi-PC <Takashi-PC@DESKTOP-6970I11>
2019-06-16 09:17:27 +0900

    最初のプロジェクト

---
+++ MyFirstApp/Calc.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyFirstApp
+{
+    public class Calc
+    {
+        /// <summary>
+        /// 整数 a + b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static int Addition(int a, int b)
+        {
+            return a + b;
+        }
+
+        /// <summary>
+        /// 整数 a - b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static int Subtraction(int a, int b)
+        {
+            return a - b;
+        }
+
+        /// <summary>
+        /// 整数 a * b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static long Multiplication(int a, int b)
+        {
+            return a * b;
+        }
+
+        /// <summary>
+        /// 整数 a ÷ b の結果を返します。
+        /// </summary>
+        /// <returns></returns>
+        public static double Division(int a, int b, int digit)
+        {
+            try
+            {
+                return System.Math.Round((double)b / a, digit);
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+    }
+}
--- MyFirstApp/Program.cs
+++ MyFirstApp/Program.cs
@@ -10,6 +10,52 @@ namespace MyFirstApp
     {
         static void Main(string[] args)
         {
+            int a = 0, b = 0;
+            Console.Title = "初めてのプログラム";
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
+
+            Console.WriteLine("整数Aを入力してください");
+            string strNumA = Console.ReadLine();
+            if (int.TryParse(strNumA, out a) == false)
+            {
+                Console.WriteLine(string.Format("数値に変換できない文字入力がありました。{0}", strNumA));
+                Console.WriteLine("終了するには、何かキーを押してください。");
+                Console.ReadLine();
+                return;
+            }
+
+
+            Console.WriteLine("整数Bを入力してください");
+            string strNumB = Console.ReadLine();
+
+            if (int.TryParse(strNumB, out b) == false)
+            {
+                Console.WriteLine(string.Format("数値に変換できない文字入力がありました。{0}", strNumB));
+                Console.WriteLine("終了するには、何かキーを押してください。");
+                Console.ReadLine();
+                return;
+            }
+            try
+            {
+                int ansAdd = Calc.Addition(a, b);
+                int ansSub = Calc.Subtraction(a, b);
+                long ansMlt = Calc.Multiplication(a, b);
+                double ansDiv = Calc.Division(a, b, 3);
+
+                Console.WriteLine(string.Format("A＋B={0}", ansAdd));
+                Console.WriteLine(string.Format("A－B={0}", ansSub));
+                Console.WriteLine(string.Format("A×B={0}", ansMlt));
+                Console.WriteLine(string.Format("A÷B={0}", ansDiv));
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            Console.WriteLine("終了するには、何かキーを押してください。");
+            Console.ReadLine();
         }
+
     }
 }
